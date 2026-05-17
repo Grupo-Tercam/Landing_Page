f3bd33e78201c3224ca5a687847f26753204c01b
david123455789 <lahoguera.4321@gmail.com>
2026-05-16 22:32:20 -0600

    new: se agrego pestana

--- JS.js
+++ JS.js
@@ -186,3 +186,16 @@ brandBubbles.forEach((bubble) => {
     bubble.classList.add("is-selected");
   });
 });
+///////////////////////////////////////////////////////////////////////////////////
+
+
+function loadHTML(id, file) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+loadHTML("header", "header.html");
+loadHTML("footer", "footer.html");
